0722b726393bfd383b324711d0b872c83050ab32
grestless <guillermodghiggia@outlook.com>
2025-06-09 12:17:36 -0300

    Conectar proyecto con github

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {Route, RouterProvider, createRoutesFromElements} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+    </Route>
+  )
+) 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store ={store}>
+    <RouterProvider router = {router} />
+  </Provider>
+  
+);
